4e07e53d899a0a1b38b0a4c09421caa1306dbd78
Theo Bonzi <theo.bonzi@epita.fr>
2022-03-04 14:21:56 -0800

    up testsuit

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -8,22 +8,77 @@ LINE="\e[1;4m"
 
 DIR="all_tests/*"
 
+touch testerror
+nbtest=0
+nberr=0
+
 #Browse all directories in tests directory
 for d in $DIR
 do
+	code_err=0
+
 	FILES="$d/*"
 	dir="${d#*/}"
 
 	echo $dir | tr '[:lower:]' '[:upper:]'
+	
+	case "$dir" in
+		"syntax")
+			code_err=3
+			;;
+		"bind")
+			code_err=4
+			;;
+		"type")
+			code_err=5
+			;;
+	esac
 
 	#Browse all files
 	for f in $FILES
 	do
-		fi="${f#tests/*/}"
-		echo Test Case $fi 
-		cat $f | grep error
+		case "$dir" in
+			"syntax")
+				code_err=3
+				;;
+			"bind")
+				code_err=4
+				;;
+			"type")
+				code_err=5
+				;;
+		esac
+		#bash ./../src/tc -X --parse $f
 
-		#TODO run test
+		if [ $? -eq $code_err ]
+		then
+			echo -n "$GREEN░$END "
+			nbtest=$(($nbtest + 1))
+		else
+			echo -n "$RED░$END "
+			nberr=$(($nberr + 1))
+			nbtest=$(($nbtest + 1))
+		fi
+		cat $f | grep error >> testerror
 	done
 	echo
+	cat testerror
+	echo -n "" > testerror
+	echo
 done
+
+echo "------------------------------------------------------------"
+echo "|                     TIGER TESTSUIT                       |"
+echo "------------------------------------------------------------"
+
+echo "$nberr / $nbtest" 
+
+rm testerror
+
+
+
+
+
+
+
+
