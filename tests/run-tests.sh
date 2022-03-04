d98631be44873a3fff9cd7e9524ec2ae29554343
Theo Bonzi <theo.bonzi@epita.fr>
2022-03-04 07:31:00 -0800

    add tests and pre commit

---
+++ tests/run-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+GREEN="\e[42m"
+RED="\e[41m"
+END="\e[0m"
+BLINK="\e[5m"
+LINE="\e[1;4m"
+
+DIR="all_tests/*"
+
+#Browse all directories in tests directory
+for d in $DIR
+do
+	FILES="$d/*"
+	dir="${d#*/}"
+
+	echo $dir | tr '[:lower:]' '[:upper:]'
+
+	#Browse all files
+	for f in $FILES
+	do
+		fi="${f#tests/*/}"
+		echo Test Case $fi 
+		cat $f | grep error
+
+		#TODO run test
+	done
+	echo
+done
