cf9539b0be3aad7e3b87145e2c1c43ee6d24630d
Theo Bonzi <theo.bonzi@epita.fr>
2022-03-10 19:31:13 +0100

    src/ast String, Seq, Object Exp DONE

--- src/ast/object-exp.cc
+++ src/ast/object-exp.cc
@@ -8,5 +8,16 @@
 
 namespace ast
 {
-  // FIXME: Some code was deleted here.
+    ObjectExp::ObjectExp(const Location& location, NameTy* type_name)
+            : Exp(location)
+              , type_name_(type_name)
+    {}
+
+    ObjectExp::~ObjectExp()
+    {
+        delete type_name_;
+    }
+
+    void ObjectExp::accept(ConstVisitor& v) const { v(*this); }
+    void ObjectExp::accept(Visitor& v) { v(*this); }
 } // namespace ast
--- src/ast/string-exp.hh
+++ src/ast/string-exp.hh
@@ -10,10 +10,21 @@
 
 namespace ast
 {
-  /// StringExp.
-  class StringExp : public Exp
-  {
-    // FIXME: Some code was deleted here.
-  };
+    /// StringExp.
+    class StringExp : public Exp
+    {
+        public:
+            StringExp(const Location& location, std::string string);
+            StringExp(const StringExp&) = delete;
+            StringExp& operator=(const StringExp&) = delete;
+
+            void accept(ConstVisitor& v) const override;
+            void accept(Visitor& v) override;
+
+            std::string string_get() const;
+
+        protected:
+            std::string string_;
+    };
 } // namespace ast
 #include <ast/string-exp.hxx>
