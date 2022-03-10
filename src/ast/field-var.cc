ce9968ed9ea4535035238cc7d560bf762e7ca46f
Jean-Sebastien Kersaint Tournebize <jean-sebastien.kersaint-tournebize@epita.fr>
2022-03-10 22:20:04 +0100

    src/ast/field-var OK

--- src/ast/field-var.cc
+++ src/ast/field-var.cc
@@ -8,5 +8,17 @@
 
 namespace ast
 {
-  // FIXME: Some code was deleted here.
+  FieldVar::FieldVar(const Location& location, Var* var, misc::symbol name)
+    : Var(location)
+    , var_(var)
+    , name_(name)
+  {}
+
+  FieldVar::~FieldVar()
+  {
+    delete var_;
+  }
+
+  void FieldVar::accept(ConstVisitor& v) const { v(*this); }
+  void FieldVar::accept(Visitor& v) { v(*this); }
 } // namespace ast
