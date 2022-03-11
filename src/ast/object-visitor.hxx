d8019562aa7cc51a43e3ba330731045c5ba730f4
Theo Bonzi <theo.bonzi@epita.fr>
2022-03-11 07:39:27 -0800

    object visitor compile pas

--- src/ast/object-visitor.hxx
+++ src/ast/object-visitor.hxx
@@ -27,31 +27,39 @@ namespace ast
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<ClassTy>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.super_get().accept();
+
+    for (auto ch : e.chunks_get())
+      ch->accept(*this);
   }
 
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodChunk>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.accept(*this);
   }
 
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodDec>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.name_get().accpet(*this);
+    e.formals_get()->accept(*this);
+    e.result_get()->accept(*this);
+    e.body_get()->accept(*this);
   }
 
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodCallExp>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.name_get().accept(*this);
+    e.args_get().accept(*this);
+    e.get_object().accept(*this);
   }
 
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<ObjectExp>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.type_name_get()->accept(*this);
   }
 
 } // namespace ast
